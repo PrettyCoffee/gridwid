7a30fd6277727db735604278cbe0922b6cbb36ef
PrettyCoffee <jonathan-holz@outlook.com>
2023-09-03 18:00:54 +0200

    chore: Setup eslint

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react"
 
-import App from './App'
+import { createRoot } from "react-dom/client"
 
-import './index.css'
+import App from "./App"
 
-const root = document.getElementById('root')
-if(!root) throw new Error("Root node could not be found")
+import "./index.css"
+
+const root = document.getElementById("root")
+if (!root) throw new Error("Root node could not be found")
 
 createRoot(root).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
