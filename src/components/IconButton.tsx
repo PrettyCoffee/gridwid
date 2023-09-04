a38c0e650a629c0305ff81baead1730fe9855172
PrettyCoffee <jonathan-holz@outlook.com>
2023-09-05 00:10:20 +0200

    feat(Button): Add press state

--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -3,22 +3,8 @@ import { forwardRef } from "react"
 import { LucideIcon } from "lucide-react"
 
 import { Button } from "~/components/ui/button"
-import { cn } from "~/lib/utils"
 
 import { ClassNameProp } from "./base/BaseProps"
-
-const pressEffect = cn(
-  "after:block",
-  "after:absolute",
-  "after:inset-0",
-  "after:bg-gradient-to-br",
-  "after:from-transparent",
-  "after:to-background/75",
-  "after:opacity-0",
-  "after:transition-opacity",
-  "after:duration-75",
-  "active:after:opacity-100"
-)
 
 export interface IconButtonProps extends ClassNameProp {
   icon: LucideIcon
@@ -32,7 +18,7 @@ export const IconButton = forwardRef<HTMLButtonElement, IconButtonProps>(
       ref={ref}
       variant="ghost"
       size="icon"
-      className={cn("relative", pressEffect, className)}
+      className={className}
       {...delegated}
     >
       <Icon absoluteStrokeWidth className="h-5" />
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -6,19 +6,21 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "~/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "relative inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 after:block after:absolute after:inset-0 after:bg-gradient-to-br after:from-transparent after:opacity-0 after:transition-opacity after:duration-75 active:after:opacity-100",
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        default:
+          "bg-primary text-primary-foreground hover:bg-primary/90 after:to-background/30",
         destructive:
-          "bg-destructive text-destructive-foreground hover:bg-destructive/90",
+          "bg-destructive text-destructive-foreground hover:bg-destructive/90 after:to-background/30",
         outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+          "border border-input bg-background hover:bg-accent hover:text-accent-foreground after:to-background/50",
         secondary:
-          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "text-primary underline-offset-4 hover:underline",
+          "bg-secondary text-secondary-foreground hover:bg-secondary/80 after:to-background/50",
+        ghost:
+          "hover:bg-accent hover:text-accent-foreground after:to-background/50",
+        link: "text-primary underline-offset-4 hover:underline active:opacity-80",
       },
       size: {
         default: "h-10 px-4 py-2",
