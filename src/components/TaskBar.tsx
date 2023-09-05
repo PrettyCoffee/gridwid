f5c559fcca119867d615e4746f82d94aa2d87d8c
PrettyCoffee <jonathan-holz@outlook.com>
2023-09-05 18:44:50 +0200

    feat(Workspaces): Enhance expand hover

--- src/components/TaskBar.tsx
+++ src/components/TaskBar.tsx
@@ -5,7 +5,7 @@ const Section = forwardRef<HTMLDivElement, PropsWithChildren>(
     return (
       <div
         ref={ref}
-        className="flex-1 flex items-center justify-center first-of-type:justify-start last-of-type:justify-end"
+        className="flex-1 flex items-center justify-center first-of-type:justify-start last-of-type:justify-end whitespace-nowrap"
       >
         {children}
       </div>
@@ -15,7 +15,7 @@ const Section = forwardRef<HTMLDivElement, PropsWithChildren>(
 
 const Root = ({ children }: PropsWithChildren) => {
   return (
-    <div className="flex items-center px-1 h-12 bg-background border-t mt-1">
+    <div className="flex items-center w-full min-w-max px-1 h-12 bg-background border-t mt-1">
       {children}
     </div>
   )
--- src/page/Workspaces.tsx
+++ src/page/Workspaces.tsx
@@ -1,9 +1,15 @@
 import { Circle } from "lucide-react"
 
 import { IconButton } from "~/components/IconButton"
+import { cn } from "~/lib/utils"
 
 export const Workspaces = () => (
-  <div className="[&:not(:hover)]:pl-2 [&:not(:hover)>*]:w-6 [&>*]:transition-all transition-all">
+  <div
+    className={cn(
+      "[&:not(:hover,:focus-within)]:pl-2 [&:not(:hover,:focus-within)>*]:w-6 [&>*]:transition-all transition-all",
+      "relative before:absolute before:-left-2 before:-bottom-2 before:-top-10 before:-right-20 before:rounded-se-full"
+    )}
+  >
     <IconButton
       icon={Circle}
       className="text-highlight-foreground [&>svg]:fill-highlight-foreground"
