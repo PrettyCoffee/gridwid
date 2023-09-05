a622b8ba3888ca610386098c020e13e90fea3e7d
PrettyCoffee <jonathan-holz@outlook.com>
2023-09-05 18:08:02 +0200

    feat(Button): Add active prop

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -28,6 +28,9 @@ const buttonVariants = cva(
         lg: "h-11 rounded-md px-8",
         icon: "h-10 w-10",
       },
+      active: {
+        true: "text-highlight-foreground hover:text-highlight-foreground",
+      },
     },
     defaultVariants: {
       variant: "default",
@@ -43,11 +46,11 @@ export interface ButtonProps
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  ({ className, variant, size, asChild = false, active, ...props }, ref) => {
     const Comp = asChild ? Slot : "button"
     return (
       <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
+        className={cn(buttonVariants({ variant, size, active, className }))}
         ref={ref}
         {...props}
       />
