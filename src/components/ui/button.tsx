c5f4ac84d11e66ad829fccdc921772a2bbdd5d8c
PrettyCoffee <jonathan-holz@outlook.com>
2023-09-08 01:46:21 +0200

    fix(IconButton): Force minimum width and height

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -30,7 +30,7 @@ const buttonVariants = cva(
         default: "h-10 px-4 py-2",
         sm: "h-9 rounded-md px-3",
         lg: "h-11 rounded-md px-8",
-        icon: "h-10 w-10",
+        icon: "h-10 w-10 min-w-[theme(width.10)] min-h-[theme(height.10)]",
       },
       active: {
         true: "text-highlight-foreground hover:text-highlight-foreground",
