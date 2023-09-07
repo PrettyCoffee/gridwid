2e77420d4d4d40f56fd05395c5175c8aa6a3487f
PrettyCoffee <jonathan-holz@outlook.com>
2023-09-07 21:19:03 +0200

    feat(IconButton): Add click animations

--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -1,30 +1,49 @@
-import { forwardRef } from "react"
+import { MouseEventHandler, forwardRef, useState } from "react"
 
 import { TooltipPortal } from "@radix-ui/react-tooltip"
 import { LucideIcon } from "lucide-react"
 
 import { Button } from "~/components/ui/button"
+import { cn } from "~/lib/utils"
 
 import { ClassNameProp } from "./base/BaseProps"
 import { Tooltip, TooltipContent, TooltipTrigger } from "./ui/tooltip"
 
+type ButtonClick = MouseEventHandler<HTMLButtonElement>
+
 export interface IconButtonProps extends ClassNameProp {
   icon: LucideIcon
   title: string
-  onClick?: () => void
+  onClick?: ButtonClick
+  clickAnimation?: string
 }
 
 export const IconButton = forwardRef<HTMLButtonElement, IconButtonProps>(
-  ({ icon: Icon, className, title, ...delegated }, ref) => {
+  (
+    { icon: Icon, className, title, onClick, clickAnimation, ...delegated },
+    ref
+  ) => {
+    const [animate, setAnimate] = useState(false)
+
+    const handleClick: ButtonClick = (...args) => {
+      onClick?.(...args)
+      if (clickAnimation) setAnimate(true)
+    }
+
     const button = (
       <Button
         ref={ref}
         variant="ghost"
         size="icon"
         className={className}
+        onAnimationEnd={() => setAnimate(false)}
+        onClick={handleClick}
         {...delegated}
       >
-        <Icon absoluteStrokeWidth className="h-5" />
+        <Icon
+          absoluteStrokeWidth
+          className={cn("h-5", animate ? clickAnimation : "")}
+        />
       </Button>
     )
     return !title ? (
