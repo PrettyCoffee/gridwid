501c4135c4f1757faed3920d10ed0f2a9e33ad0f
PrettyCoffee <jonathan-holz@outlook.com>
2023-09-14 00:33:49 +0200

    feat(Button): Add compactIcon size

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -32,7 +32,9 @@ const buttonVariants = cva(
         default: "h-10 px-4 py-2",
         sm: "h-9 rounded-md px-3",
         lg: "h-11 rounded-md px-8",
-        icon: "h-10 w-10 min-w-[theme(width.10)] min-h-[theme(height.10)]",
+        icon: "h-10 w-10 min-w-[theme(width.10)] min-h-[theme(height.10)] max-w-[theme(width.10)] max-h-[theme(height.10)]",
+        compactIcon:
+          "h-8 w-8 min-w-[theme(width.8)] min-h-[theme(height.8)] max-w-[theme(width.8)] max-h-[theme(height.8)]",
       },
       active: {
         true: "text-highlight-foreground hover:text-highlight-foreground",
