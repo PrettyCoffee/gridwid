605830594af8e051f907be35f7718848b20084cc
PrettyCoffee <jonathan-holz@outlook.com>
2023-09-17 22:16:06 +0200

    feat(tailwind): Calculate border sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -52,8 +52,8 @@ module.exports = {
       },
       borderRadius: {
         lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
+        md: "calc(var(--radius) * 0.75)",
+        sm: "calc(var(--radius) * 0.5)",
       },
       keyframes: {
         "accordion-down": {
