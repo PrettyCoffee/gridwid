0a7025bd6d422eb001e0688426d5b88fe95605e8
PrettyCoffee <jonathan-holz@outlook.com>
2023-09-21 01:42:26 +0200

    feat(Dialog): Change open animation direction

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -40,7 +40,7 @@ const dialogContent = cva(
     "fixed overflow-y-auto z-50 border bg-background shadow-lg",
     "inset-2 p-6 m-auto w-auto h-max max-h-[calc(100vh-theme(height.4))]",
     "sm:inset-8 sm:max-h-[calc(100vh-theme(height.16))] sm:rounded-lg",
-    "duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]"
+    "duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-bottom-[48%] data-[state=open]:slide-in-from-bottom-[48%]"
   ),
   {
     variants: {
