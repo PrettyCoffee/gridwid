06994264badf0a53b96c170a690770c83f0f0cc4
PrettyCoffee <jonathan-holz@outlook.com>
2023-10-05 00:30:13 +0200

    feat(Textarea): Add component

---
+++ src/components/ui/textarea.tsx
@@ -0,0 +1,25 @@
+import * as React from "react"
+
+import { focusRing } from "~/lib/styles"
+import { cn } from "~/lib/utils"
+
+export type TextareaProps = React.TextareaHTMLAttributes<HTMLTextAreaElement>
+
+const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <textarea
+        className={cn(
+          "flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50",
+          focusRing,
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
+    )
+  }
+)
+Textarea.displayName = "Textarea"
+
+export { Textarea }
