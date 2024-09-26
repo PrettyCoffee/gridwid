a74cbb46ff8d474225a5201b392e4c36f0176412
PrettyCoffee <66521452+PrettyCoffee@users.noreply.github.com>
2024-09-26 19:32:31 +0200

    fix(list): Force correct item content size

--- src/components/ui/list/list.tsx
+++ src/components/ui/list/list.tsx
@@ -21,7 +21,7 @@ const Item = ({
     className={cn(
       hstack({}),
       active ? "border-text-highlight/50" : "border-text-gentle/10",
-      "bgl-base-b/10 hover:bgl-layer-w/5 h-10 list-none rounded border [&>*]:rounded-none [&>:first-child]:rounded-l [&>:last-child]:rounded-e",
+      "bgl-base-b/10 hover:bgl-layer-w/5 h-10 list-none rounded border [&>*]:h-full [&>*]:rounded-none [&>:first-child]:rounded-l [&>:last-child]:rounded-e",
       className
     )}
   >
