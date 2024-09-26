6b915faf95c35d5404f30cbab70f39d2d8075831
PrettyCoffee <66521452+PrettyCoffee@users.noreply.github.com>
2024-09-26 22:58:40 +0200

    feat(format): Allow numbers in date formatters

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,10 +1,10 @@
-export const formatTime = (date: Date) =>
-  date.toLocaleTimeString(undefined, {
+export const formatTime = (date: Date | number) =>
+  new Date(date).toLocaleTimeString(undefined, {
     hour: "2-digit",
     minute: "2-digit",
   })
-export const formatDate = (date: Date) =>
-  date.toLocaleDateString(undefined, {
+export const formatDate = (date: Date | number) =>
+  new Date(date).toLocaleDateString(undefined, {
     day: "2-digit",
     month: "2-digit",
     year: "numeric",
