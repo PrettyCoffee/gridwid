04a534ef7f4ab778801fface2083e58947f71b91
PrettyCoffee <jonathan-holz@outlook.com>
2024-10-23 01:39:28 +0200

    feat(nodes-sidebar): Make notes sortable

--- src/app/routes/notes/notes-sidebar.tsx
+++ src/app/routes/notes/notes-sidebar.tsx
@@ -1,5 +1,6 @@
 import { useAtomValue } from "@yaasl/react"
 import { GripHorizontal, Plus, Trash } from "lucide-react"
+import { forwardRef } from "react"
 
 import { Layout } from "components/layouts"
 import { Button } from "components/ui/button"
@@ -7,26 +8,43 @@ import { Divider } from "components/ui/divider"
 import { IconButton } from "components/ui/icon-button"
 import { List } from "components/ui/list"
 import { useHashRouter } from "components/utility/hash-router"
+import { Sortable } from "components/utility/sortable"
 import { createRandomNote, Note, notesData } from "features/notes"
 import { deleteNote } from "features/notes/delete-note"
+import { cn } from "utils/cn"
 
-const ListItem = ({ id, active, title }: Note & { active?: boolean }) => (
-  <List.Item active={active}>
-    <IconButton
-      icon={GripHorizontal}
-      title="Re-order note"
-      hideTitle
-      className="cursor-grab active:cursor-grabbing"
-    />
-    <List.Label to={`notes/${id}`}>{title}</List.Label>
+const ListItem = forwardRef<
+  HTMLLIElement,
+  {
+    note: Note
+    active?: boolean
+    isPlaceholder?: boolean
+  }
+>(({ active, isPlaceholder, note, ...props }, ref) => (
+  <List.Item
+    {...props}
+    ref={ref}
+    active={active}
+    className={cn(isPlaceholder && "[&_*]:opacity-0")}
+  >
+    <Sortable.Handle item={note} asChild>
+      <IconButton
+        icon={GripHorizontal}
+        title="Re-order note"
+        hideTitle
+        className="cursor-grab active:cursor-grabbing"
+      />
+    </Sortable.Handle>
+
+    <List.Label to={`notes/${note.id}`}>{note.title}</List.Label>
     <List.Action
       icon={Trash}
       title="Delete note"
       hideTitle
-      onClick={() => deleteNote(id, title)}
+      onClick={() => deleteNote(note.id, note.title)}
     />
   </List.Item>
-)
+))
 
 const getNoteById = (notes: Note[], id?: string) =>
   !id ? undefined : notes.find(note => note.id === id)
@@ -55,15 +73,33 @@ export const NotesSidebar = () => {
       <Divider color="gentle" />
 
       <div className="-m-1 flex-1 overflow-auto p-1">
-        <List.Root>
-          {notes.map(note => (
-            <ListItem
-              {...note}
-              key={note.id}
-              active={note.id === currentNote?.id}
-            />
-          ))}
-        </List.Root>
+        <Sortable.Context<Note>
+          items={notes}
+          onSort={sort => notesData.set(sort(notesData.get()))}
+          OverlayItem={({ item }) => (
+            <div className="bg-background-page shade-medium">
+              <ListItem
+                note={item}
+                key={item.id}
+                active={item.id === currentNote?.id}
+              />
+            </div>
+          )}
+        >
+          <List.Root>
+            {notes.map(note => (
+              <Sortable.Item<Note> key={note.id} item={note} asChild>
+                {({ isDragging }) => (
+                  <ListItem
+                    note={note}
+                    isPlaceholder={isDragging}
+                    active={note.id === currentNote?.id}
+                  />
+                )}
+              </Sortable.Item>
+            ))}
+          </List.Root>
+        </Sortable.Context>
       </div>
     </Layout.Side>
   )
