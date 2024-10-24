79928cb95bbd59ad3a3e79437d2a1095ec044b51
PrettyCoffee <jonathan-holz@outlook.com>
2024-10-24 22:19:12 +0200

    feat(notes-preview): Add card hover effect

--- src/features/notes/note-preview.tsx
+++ src/features/notes/note-preview.tsx
@@ -1,3 +1,6 @@
+import { PenBox } from "lucide-react"
+
+import { Icon } from "components/ui/icon"
 import { HashRouter } from "components/utility/hash-router"
 import { cn } from "utils/cn"
 import { formatDate } from "utils/format"
@@ -12,10 +15,24 @@ export const NotePreview = ({
   createdAt,
   changedAt,
 }: Note) => (
-  <div className={cn(surface({ look: "card", size: "lg" }), "size-full")}>
+  <div
+    className={cn(
+      surface({ look: "card", size: "lg" }),
+      "hover:border-stroke/50 size-full [&:not(:hover)>*]:opacity-80"
+    )}
+  >
     <HashRouter.Link to={`notes/${id}`}>
       <h2 className="text-text-priority text-2xl underline-offset-4 hover:underline">
         {title}
+        <span className="relative [*:not(:hover)>*>*>&]:hidden">
+          <Icon
+            icon={PenBox}
+            size="xs"
+            color="gentle"
+            strokeWidth={2.5}
+            className="absolute -right-3 top-0"
+          />
+        </span>
       </h2>
     </HashRouter.Link>
     <span className="text-text-gentle text-sm">
