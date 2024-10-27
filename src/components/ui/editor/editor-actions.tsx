a50bcb27d3b70868b68d4487e64059cce539e09f
PrettyCoffee <jonathan-holz@outlook.com>
2024-10-27 23:00:49 +0100

    feat(Editor): Disable save action if a field is invalid

--- src/components/ui/editor/editor-actions.tsx
+++ src/components/ui/editor/editor-actions.tsx
@@ -7,28 +7,30 @@ import { useEditorContext } from "./editor-context"
 
 export const EditorSave = () => {
   const editor = useEditorContext()
+  const disabled = !editor.didChange || !editor.isValid
 
   return (
     <IconButton
       icon={Save}
       title="Save changes"
-      onClick={() => editor.save()}
-      disabled={!editor.didChange}
-      className={cn(!editor.didChange && "opacity-10")}
+      onClick={editor.save}
+      disabled={disabled}
+      className={cn(disabled && "opacity-25")}
     />
   )
 }
 
 export const EditorDiscard = () => {
   const editor = useEditorContext()
+  const disabled = !editor.didChange
 
   return (
     <IconButton
       icon={Undo}
       title="Discard changes"
-      onClick={() => editor.discard()}
-      disabled={!editor.didChange}
-      className={cn(!editor.didChange && "opacity-10")}
+      onClick={editor.discard}
+      disabled={disabled}
+      className={cn(disabled && "opacity-25")}
     />
   )
 }
