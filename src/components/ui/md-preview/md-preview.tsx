d0553049756abecaa02e042acbae676f3777dbfb
PrettyCoffee <jonathan-holz@outlook.com>
2024-12-15 18:17:41 +0100

    fix(MDPreview): Remove excessive margin from li elements

--- src/components/ui/md-preview/md-preview.tsx
+++ src/components/ui/md-preview/md-preview.tsx
@@ -14,7 +14,10 @@ export interface MDPreviewProps extends ClassNameProp {
 export const MDPreview = ({ value = "", className }: MDPreviewProps) => {
   return (
     <div
-      className={cn("prose dark:prose-invert prose-zinc", className)}
+      className={cn(
+        "prose dark:prose-invert prose-zinc [&_li>p]:m-0 [&_li]:my-1",
+        className
+      )}
       dangerouslySetInnerHTML={{
         __html: parse(adjustForRender(value)) as string,
       }}
