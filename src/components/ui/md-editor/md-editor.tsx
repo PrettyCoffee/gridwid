5eaf943804920515502bbc314972a929c51146de
PrettyCoffee <jonathan-holz@outlook.com>
2024-12-15 20:56:51 +0100

    feat(MDEditor): Replace mode state buttons with slider

--- src/components/ui/md-editor/md-editor.tsx
+++ src/components/ui/md-editor/md-editor.tsx
@@ -1,14 +1,102 @@
 import { Slot } from "@radix-ui/react-slot"
-import { Code, LetterText, SquareSplitHorizontal } from "lucide-react"
-import { PropsWithChildren, useState } from "react"
+import { css } from "goober"
+import { Code, LetterText } from "lucide-react"
+import { Dispatch, PropsWithChildren, useState } from "react"
 
 import { AsChildProp, ClassNameProp, StyleProp } from "types/base-props"
 import { cn } from "utils/cn"
 import { hstack, vstack } from "utils/styles"
 
 import { CodeEditor, CodeEditorProps } from "../code-editor"
+import { IconButton } from "../icon-button"
 import { MDPreview } from "../md-preview"
-import { StateSwitch } from "../state-switch"
+
+const trackStyles = `
+  color: inherit;
+  background: currentColor;
+  height: 0.25rem;
+  border-radius: 50vh;
+`
+
+const thumbStyles = `
+  color: inherit;
+  appearance: none;
+  height: var(--slider-size);
+  width: var(--slider-size);
+  background: black;
+  outline: 2px solid currentColor;
+  margin-top: -0.4rem;
+  border-radius: 50%;
+  border: none;
+`
+
+const slider = css`
+  --slider-size: 1rem;
+
+  appearance: none;
+  display: inline-block;
+  background: transparent;
+  width: 100%;
+  cursor: pointer;
+  height: 1.5rem;
+  border-radius: 0.25rem;
+
+  &::-moz-range-track {
+    ${trackStyles}
+  }
+  &::-moz-range-thumb {
+    ${thumbStyles}
+  }
+
+  input[type="range"]& {
+    &::-webkit-slider-runnable-track {
+      ${trackStyles}
+    }
+    &::-webkit-slider-thumb {
+      ${thumbStyles}
+    }
+  }
+`
+
+const ModeSlider = ({
+  value,
+  onChange,
+}: {
+  value: number
+  onChange: Dispatch<number>
+}) => {
+  return (
+    <div
+      className={cn(
+        hstack({ gap: 1, align: "center" }),
+        "bg-background/75 border-stroke w-36 rounded-full border backdrop-blur-sm"
+      )}
+    >
+      <IconButton
+        title="Show preview"
+        icon={LetterText}
+        size="sm"
+        className="rounded-full"
+        onClick={() => onChange(0)}
+      />
+      <input
+        type="range"
+        min="20"
+        max="80"
+        value={value}
+        onChange={({ target }) => onChange(Number(target.value))}
+        className={cn("text-stroke-button/75 hover:text-stroke-button", slider)}
+      />
+      <IconButton
+        title="Show code"
+        icon={Code}
+        size="sm"
+        className="rounded-full"
+        onClick={() => onChange(100)}
+      />
+    </div>
+  )
+}
 
 const ScrollArea = ({
   className,
@@ -33,66 +121,44 @@ export interface MDEditorProps
 
 export const MDEditor = ({ className, ...inputProps }: MDEditorProps) => {
   const { value } = inputProps
-  const [mode, setMode] = useState<EditorMode>("split")
+  const [modeValue, setModeValue] = useState(50)
 
-  const codeWidth = { code: 100, split: 50, preview: 0 }[mode]
-  const previewWidth = { code: 0, split: 50, preview: 100 }[mode]
+  const codeWidth = modeValue
+  const previewWidth = 100 - modeValue
+
+  const showCode = codeWidth > 20
+  const showPreview = previewWidth > 20
 
   return (
     <div
       className={cn(
-        hstack({ gap: 0 }),
+        hstack({}),
         "relative size-full flex-1 overflow-hidden",
         className
       )}
     >
-      <div className="absolute right-1 top-1 z-50">
-        <StateSwitch.Group
-          current={mode}
-          onChange={value => setMode(value as EditorMode)}
-          size="sm"
-        >
-          <StateSwitch.Option label={"Code"} value={"code"} icon={Code} />
-          <StateSwitch.Option
-            label={"Split view"}
-            value={"split"}
-            icon={SquareSplitHorizontal}
-          />
-          <StateSwitch.Option
-            label={"Preview"}
-            value={"preview"}
-            icon={LetterText}
-          />
-        </StateSwitch.Group>
+      <div className="absolute right-2 top-1 z-50">
+        <ModeSlider value={modeValue} onChange={setModeValue} />
       </div>
 
       <ScrollArea
-        className={cn(
-          "transition-all duration-300",
-          mode === "split" ? "pr-2" : "pr-0"
-        )}
-        style={{
-          flexGrow: codeWidth ? 1 : 0,
-          flexShrink: codeWidth ? 1 : 0,
-          flexBasis: `${codeWidth}%`,
-        }}
+        className={cn(!showCode && "hidden")}
+        style={{ flex: `1 1 ${codeWidth}%` }}
       >
         <CodeEditor {...inputProps} language="markdown" />
       </ScrollArea>
 
       <ScrollArea
-        asChild
-        className={cn(
-          "transition-all duration-300",
-          mode === "split" ? "pl-2" : "pl-0"
-        )}
-        style={{
-          flexGrow: previewWidth ? 1 : 0,
-          flexShrink: previewWidth ? 1 : 0,
-          flexBasis: `${previewWidth}%`,
-        }}
+        className={cn(!showPreview && "hidden")}
+        style={{ flex: `1 1 ${previewWidth}%` }}
       >
-        <MDPreview value={value} className="max-w-none" />
+        <MDPreview
+          value={value}
+          className={cn(
+            "max-w-none pl-4 pt-1",
+            showCode && "border-stroke-gentle ml-4 border-l"
+          )}
+        />
       </ScrollArea>
     </div>
   )
