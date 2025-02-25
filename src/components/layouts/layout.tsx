85422fd2bb4ecaf0b7a0a2e5f5f08d269900baa9
PrettyCoffee <jonathan-holz@outlook.com>
2025-02-26 00:08:30 +0100

    fix(Layout.Side): Fix content scroll area when overflowing

--- src/components/layouts/layout.tsx
+++ src/components/layouts/layout.tsx
@@ -49,36 +49,41 @@ const Side = ({ children, back, className }: LayoutSideProps) => {
   return (
     <div
       className={cn(
-        vstack({ gap: 2, align: "stretch" }),
-        "relative -m-2 h-[calc(100%+theme(spacing.4))] overflow-hidden p-2",
+        vstack(),
+        "relative -m-2 h-[calc(100%+theme(spacing.4))] p-2",
         "transition-[max-width,min-width] duration-300 ease-in-out [&>*:not(:last-child)]:transition-opacity [&>*:not(:last-child)]:duration-300",
         isOpen
           ? "min-w-64 max-w-64 [&>*:not(:last-child)]:opacity-100"
           : "min-w-16 max-w-16 [&>*:not(:last-child)]:opacity-0",
-        animate.state !== "open" && "overflow-x-hidden [&_*]:overflow-x-hidden",
+        animate.state !== "open" && "overflow-hidden [&_*]:overflow-hidden",
         "border-stroke-gentle mr-0 border-r pr-4",
         className
       )}
     >
-      {animate.mounted && back && (
-        <>
-          <Button to={back.path} icon={ArrowLeft} className="justify-start">
-            {back.caption}
-          </Button>
-          <Divider color="gentle" />
-        </>
-      )}
+      <div
+        className={cn(
+          vstack({ gap: 2, align: "stretch" }),
+          "-m-2 flex-1 overflow-hidden p-2"
+        )}
+      >
+        {animate.mounted && back && (
+          <>
+            <Button to={back.path} icon={ArrowLeft} className="justify-start">
+              {back.caption}
+            </Button>
+            <Divider color="gentle" />
+          </>
+        )}
 
-      {animate.mounted && children}
-
-      <div className="flex-1" />
+        {animate.mounted && children}
+      </div>
 
       <IconButton
         title={isOpen ? "Collapse side menu" : "Expand side menu"}
         titleSide="right"
         onClick={isOpen ? close : open}
         icon={isOpen ? ArrowLeftFromLine : ArrowRightToLine}
-        className="ml-auto"
+        className="ml-auto mt-4"
       />
     </div>
   )
