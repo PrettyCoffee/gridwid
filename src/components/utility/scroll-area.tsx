88834330c5b2b19aea7783d0d0e1ac1605e3bd46
PrettyCoffee <jonathan-holz@outlook.com>
2025-03-01 01:04:08 +0100

    feat(ScrollArea): Add ref prop

--- src/components/utility/scroll-area.tsx
+++ src/components/utility/scroll-area.tsx
@@ -1,4 +1,4 @@
-import { PropsWithChildren } from "react"
+import { forwardRef, PropsWithChildren } from "react"
 
 import { Slot } from "@radix-ui/react-slot"
 
@@ -6,16 +6,25 @@ import { AsChildProp, ClassNameProp, StyleProp } from "types/base-props"
 import { cn } from "utils/cn"
 import { vstack } from "utils/styles"
 
-export const ScrollArea = ({
-  className,
-  children,
-  asChild,
-  style,
-}: PropsWithChildren<ClassNameProp & AsChildProp & StyleProp>) => {
+interface ScrollAreaProps extends ClassNameProp, AsChildProp, StyleProp {
+  onScroll?: () => void
+}
+
+export const ScrollArea = forwardRef<
+  HTMLDivElement,
+  PropsWithChildren<ScrollAreaProps>
+>(({ className, children, asChild, style, onScroll }, ref) => {
   const Inner = asChild ? Slot : "div"
   return (
     <div style={style} className={cn("overflow-hidden", vstack({}), className)}>
-      <Inner className="w-full flex-1 overflow-auto">{children}</Inner>
+      <Inner
+        ref={ref}
+        onScroll={onScroll}
+        className="w-full flex-1 overflow-auto"
+      >
+        {children}
+      </Inner>
     </div>
   )
-}
+})
+ScrollArea.displayName = "ScrollArea"
