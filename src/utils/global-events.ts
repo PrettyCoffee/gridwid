88a7a9b24d3ff56f57cbf16b85c62b78135dbdbc
PrettyCoffee <jonathan-holz@outlook.com>
2025-03-13 22:56:29 +0100

    feat(globalEvents): Add atoms to track keys that are held down

--- src/utils/global-events.ts
+++ src/utils/global-events.ts
@@ -1,16 +1,41 @@
+import { createAtom } from "@yaasl/react"
+
 import { KeyEventDispatcher } from "./key-event-dispatcher"
 
 type Handler = () => void
 
 const saveHandlers = new Set<Handler>()
 
+const altKey = createAtom({
+  name: "alt-key",
+  defaultValue: false,
+})
+
+const ctrlKey = createAtom({
+  name: "ctrl-key",
+  defaultValue: false,
+})
+
+const shiftKey = createAtom({
+  name: "shift-key",
+  defaultValue: false,
+})
+
 const dispatcher = new KeyEventDispatcher({})
-  .beforeAll(({ event }) => event.preventDefault())
   .listen({
     key: "s",
+    type: "keydown",
     filter: event => event.ctrlKey,
-    handler: () => {
+    handler: ({ event }) => {
+      event.preventDefault()
       saveHandlers.forEach(handler => handler())
+    },
+  })
+  .listen({
+    handler: event => {
+      ctrlKey.set(event.event.ctrlKey)
+      altKey.set(event.event.altKey)
+      shiftKey.set(event.event.shiftKey)
     },
   })
 
@@ -19,7 +44,18 @@ window.addEventListener("keydown", (event: KeyboardEvent) => {
   dispatcher.emit(event)
 })
 
+window.addEventListener("keyup", (event: KeyboardEvent) => {
+  // @ts-expect-error -- React event vs native event does not matter here
+  dispatcher.emit(event)
+})
+
 export const globalEvents = {
+  keys: {
+    ctrl: ctrlKey,
+    alt: altKey,
+    shift: shiftKey,
+  },
+
   onSave: {
     subscribe: (handler: Handler) => {
       saveHandlers.add(handler)
