c1a060bfbd1b9b35b729042df547adf55a8125fe
PrettyCoffee <jonathan-holz@outlook.com>
2025-04-06 21:11:38 +0200

    fix(FileInput): Add missing index exports

--- src/components/ui/file-input/file-input.tsx
+++ src/components/ui/file-input/file-input.tsx
@@ -13,7 +13,7 @@ import {
 import { Icon } from "../icon"
 import { TitleTooltip } from "../tooltip"
 
-interface FileInputProps extends ClassNameProp {
+export interface FileInputProps extends ClassNameProp {
   label?: string
   alert?: { kind: AlertKind; text: string }
   onChange?: (value: File) => void
