a114540798b4fa741df283e2f97f7a0bfd01c099
PrettyCoffee <66521452+PrettyCoffee@users.noreply.github.com>
2025-07-28 22:17:30 +0200

    feat(toggle): Decrease slider size and improve colors

--- src/components/ui/toggle/toggle.tsx
+++ src/components/ui/toggle/toggle.tsx
@@ -12,26 +12,31 @@ export interface ToggleProps extends ClassNameProp {
   onChange: Dispatch<boolean>
 }
 
+const toggleTransition = cn(
+  "transition-[translate,background-color] duration-200 ease-out"
+)
+
 export const Toggle = ({ checked, onChange, label }: ToggleProps) => (
   <label
     className={cn(
       hstack({ gap: 2, align: "center", inline: true }),
       interactive({ look: "flat" }),
-      "h-10 cursor-pointer truncate rounded-l-lg rounded-r-md p-1 pr-3 text-start text-sm"
+      "h-10 cursor-pointer truncate rounded-l-lg rounded-r-md p-1 pr-3 pl-2 text-start text-sm"
     )}
   >
     <Primitive.Root
       checked={checked}
       onCheckedChange={onChange}
       className={cn(
-        "relative inline-block h-8 w-12 shrink-0 cursor-pointer rounded-xl",
-        "bg-background-button/15 transition-[background-color] duration-100 ease-out",
+        toggleTransition,
+        "relative inline-block h-6 w-10 shrink-0 cursor-pointer rounded-xl bg-background-button/25",
         checked && "bg-highlight/50"
       )}
     >
       <Primitive.Thumb
         className={cn(
-          "m-1 inline-block size-6 rounded-xl bg-background-button/75 transition-transform duration-100 ease-out",
+          toggleTransition,
+          "m-0.5 inline-block size-5 rounded-xl bg-background-page/75",
           checked ? "translate-x-2" : "-translate-x-2"
         )}
       />
